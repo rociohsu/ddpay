f841a22ea63db985611e03a447268add0fe2da39
Rocio Hsu <shachien@gmail.com>
2024-07-02 22:50:22 +0800

    240702 update
    新增APP安裝提示

--- src/views/Welcome/indexView.vue
+++ src/views/Welcome/indexView.vue
@@ -24,6 +24,33 @@ import "./style.less";
             <a class="button" href="#">前往申請</a>
         </div>
 
+        <!-- Android -->
+        <section class="pop pop--full pop--app" style="display: flex;">
+			<div class="pop__body">
+                <span class="pop__logo"><img src="@/assets/imgs/ddpay.png"></span>
+				<span class="pop__title">安裝DDPay後台？</span>
+				<span class="pop__content">安裝到您的首頁，以享有最完整之功能</span>
+
+				<div class="pop__actions">
+					<button class="pop__action pop__action--minor">暫時不要</button>
+					<button class="pop__action pop__action--green">加到首頁</button>
+				</div>
+			</div>
+		</section>
+
+        <!-- iOS -->
+        <section class="pop pop--full pop--app pop--ios" style="display: flex;">
+			<div class="pop__body">
+				<a class="pop__close"><i class="fa-solid fa-xmark fa-lg"></i></a>
+                <span class="pop__logo"><img src="@/assets/imgs/ddpay.png"></span>
+				<span class="pop__title">安裝DDPay後台？</span>
+				<span class="pop__content">
+                    安裝到您的首頁，以享有最完整之功能<br>
+                    按下Safari分享選單<i class="icon-share"></i>，點選「新增至主畫面」
+                </span>
+			</div>
+		</section>
+
         <RouterView />
     </main>
 </template>
